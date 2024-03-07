3b7a82ccb581f32b1e804f2a255dd6de8b815314
gudangcoding <dientrimedia@gmail.com>
2024-03-07 16:12:24 +0700

    komit2

---
+++ resources/views/layouts/content.blade.php
@@ -0,0 +1,6 @@
+<div class="page-wrapper">
+    <div class="page-body">
+        <div class="container-xl d-flex flex-column justify-content-center">
+            @yield('content')
+        </div>
+    </div>
---
+++ resources/views/layouts/menuutama.blade.php
@@ -0,0 +1,42 @@
+<header class="navbar-expand-md">
+    <div class="collapse navbar-collapse" id="navbar-menu">
+        <div class="navbar">
+            <div class="container-xl">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                            </span>
+                            <span class="nav-link-title">
+                                Home
+                            </span>
+                        </a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#navbar-help" data-bs-toggle="dropdown"
+                            data-bs-auto-close="outside" role="button" aria-expanded="false">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+
+                            </span>
+                            <span class="nav-link-title">
+                                System
+                            </span>
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="" target="_blank" rel="noopener">
+                                Hak Akses
+                            </a>
+                            <a class="dropdown-item" href="">
+                                Crud Generator
+                            </a>
+
+
+                        </div>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+</header>
